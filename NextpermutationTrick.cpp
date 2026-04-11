9efb1cf075302d06fc20eac78ff2c01a472b3263
Bhavish2005 <pushkarna.bhavish2005@gmail.com>
2026-04-11 19:26:04 +0530

    new Technique for next Permutation

---
+++ NextpermutationTrick.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int breakp=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+            breakp=i;
+            break;
+            }
+        }
+        if(breakp==-1){
+        reverse(nums.begin(), nums.end());
+        return ;
+    }
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[breakp]){
+            swap(nums[i], nums[breakp]);
+            break;
+        }
+        }
+        reverse(nums.begin()+breakp+1,nums.end());
+    }
+};
