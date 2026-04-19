5517723ac5f4011cde77ef6820124a95a62f606a
Bhavish2005 <pushkarna.bhavish2005@gmail.com>
2026-04-19 09:44:47 +0530

    19 April question done

---
+++ Normal DPP/19April.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i=nums1.size()-1;
+        int j=nums2.size()-1;
+        int ans=0;
+        while(i>=0 && j>=0)
+        {
+            if(nums1[i]<=nums2[j]){
+                ans=max(ans, j-i);
+                i--;
+            }
+            else{
+                if(i<j)
+                         j--;
+                
+                else
+                {
+                    j--;
+                    i--;
+                }
+           
+            }
+        }
+        return ans ;
+    }
+};
